c5ac36f1fa406fe651bdaa45254c5e1220016ef7
Rajesh Burla <raburla@microsoft.com>
2020-01-25 21:34:42 +0530

    Added app name

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -4,7 +4,7 @@ import { connect } from "react-redux";
 const Home = () => (
   <div>
     <h1>Hello, developer!</h1>
-    <p>This is a micro frontend app created with micro-frontend-starter.</p>
+    <p>This is a mfe1 micro frontend app created with micro-frontend-starter.</p>
     <p>It is configured with React, Redux and Sass.</p>
     <p>To learn more about micro frontends, click <a href="https://martinfowler.com/articles/micro-frontends.html">here</a> </p>
   </div>
